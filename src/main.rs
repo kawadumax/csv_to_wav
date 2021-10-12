05257cbcddd2e8c64fc54400a4f462af8b9a7009
Kazuma Kondo <kazmax7@gmail.com>
2021-10-12 16:58:00 +0900

    wavフォルダをignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    use std::fs::File;
+    use std::path::Path;
+
+    let mut inp_file = File::open(Path::new("data/sine.wav"))?;
+    let (header, data) = wav::read(&mut inp_file)?;
+
+    let mut out_file = File::create(Path::new("data/output.wav"))?;
+    wav::write(header, &data, &mut out_file)?;
 }
