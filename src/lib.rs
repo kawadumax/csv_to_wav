dacdb7cc87369d53aa6093e9aca1ff60c3fb9d91
Kazuma Kondo <kazmax7@gmail.com>
2021-11-15 18:07:51 +0900

    CSVからサイン波を合成することに成功

--- src/lib.rs
+++ src/lib.rs
@@ -93,4 +93,43 @@ mod tests {
         wav::write(header, &data, &mut out_file).expect("Unable to write file");
         Ok(())
     }
+
+    #[test]
+    fn generate_sine_csv() -> std::io::Result<()> {
+        // create a file called data.csv, overwriting the file if it already existed
+        // let mut csv = std::fs::File::create("csv/sine_data.csv");
+        use std::f32::consts::PI;
+        let mut wtr = csv::Writer::from_path("csv/sine_data.csv")?;
+
+        for i in 0..44100 {
+            let v_440 = (i as f32 * 440.0 * 2.0 * PI / 44100 as f32).sin() * i16::MAX as f32;
+            let v_880 = (i as f32 * 880.0 * 2.0 * PI / 44100 as f32).sin() * i16::MAX as f32;
+            wtr.write_record(&[v_440.to_string(), v_880.to_string()])?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn generate_sine_wav_from_csv() -> std::io::Result<()> {
+        let mut rdr = csv::ReaderBuilder::new().from_path("csv/sine_data.csv")?;
+        let mut raw_data = Vec::with_capacity(44100);
+        for result in rdr.records() {
+            let record = result.expect("a CSV record");
+            // stringRecordをi16に変換したい
+            // dbg!(record.to_owned());
+            let record = record.get(0).unwrap();
+            // dbg!(record);
+            let sine_data: f32 = record.parse().unwrap();
+
+            raw_data.push(sine_data as i16);
+        }
+        let data: wav::BitDepth = wav::BitDepth::Sixteen(raw_data);
+        let header = wav::Header::new(wav::header::WAV_FORMAT_PCM, 1, 44100, 16);
+
+        let mut out_file = std::fs::File::create(std::path::Path::new("wav/sine_output.wav"))
+            .expect("Unable to create file");
+        wav::write(header, &data, &mut out_file).expect("Unable to write file");
+
+        Ok(())
+    }
 }
